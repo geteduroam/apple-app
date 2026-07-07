9dca289e011cce48c6f8b4bf281356589aa984e4
Johan Kool <johankool@users.noreply.github.com>
2026-07-07 17:18:18 +0200

    Try disabling explicit module builds

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,3 +6,9 @@ set -e
 
 # Allow usage of macros
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
+
+# Work around Xcode Cloud "Unable to resolve module dependency" errors for
+# SwiftSyntax-based macro plugins (swift-perception, swift-dependencies,
+# swift-composable-architecture, swift-case-paths, app-remote-config) by
+# disabling explicit module builds for this build session.
+defaults write com.apple.dt.Xcode IDEBuildingExplicitModules -bool NO
